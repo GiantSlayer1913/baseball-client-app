d6e196bbf796f67d6ae0f463847043a8dba03f51
GiantSlayer1913 <michaeljaredfisch@gmail.com>
2018-04-26 15:11:10 -0400

    Head: committing changes Body: heroku api set correctly

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://git.heroku.com/baseball-database.git',
+  production: 'https://baseball-database.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
